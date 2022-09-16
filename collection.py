c4f81e2c9de7293a80998f765a17821294332259
Ganeshvallipi <112767562+Ganeshvallipi@users.noreply.github.com>
2022-09-16 08:20:07 +0530

    Add files via upload

---
+++ collection.py
@@ -0,0 +1,10 @@
+def permutations(s,i=0)
+    if i==len(s);
+       print("",join(s))
+  for j in range(i,len(s));
+      kai=[c for c in s]
+      kai[i],kai[j]=kai[j],kai[i]
+      permutation(kai,i + 1)
+
+a=str(input("enter the numbers"))
+print(","permutation(a))
